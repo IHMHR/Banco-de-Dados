94f39607ade528cdae69c3e166d8bfc385de0a27
IHMHR <martinelli.igor@hotmail.com>
2017-05-03 10:05:10 -0300

    Ajuste

--- lab03.sql
+++ lab03.sql
@@ -12,7 +12,7 @@
 
 6) SELECT num_matricula, nom_empregado, nom_depto, num_matricula_gerente FROM bd_empresa..empregado e INNER JOIN bd_empresa..departamento d ON e.cod_depto = d.cod_depto;
 
-****7) SELECT num_matricula_supervisor, nom_empregado FROM bd_empresa..empregado e INNER JOIN bd_empresa..;
+7) SELECT e.num_matricula_supervisor, e1.nom_empregado FROM empregado e INNER JOIN empregado e1 ON e.num_matricula_supervisor = e1.num_matricula GROUP BY e.num_matricula_supervisor, e1.nom_empregado;
 
 8) SELECT e.nom_empregado, s.nom_empregado AS supervisor FROM bd_empresa..empregado e LEFT JOIN bd_empresa..empregado s ON s.num_matricula = e.num_matricula_supervisor;
 
@@ -22,9 +22,12 @@
 
 11) SELECT nom_depto, nom_empregado FROM bd_empresa..departamento d LEFT JOIN bd_empresa..empregado e ON d.cod_depto = e.cod_depto;
 
-****12) SELECT p.cod_projeto, nom_projeto FROM bd_empresa..projeto p FULL OUTER JOIN bd_empresa..alocacao a ON p.cod_projeto = a.cod_projeto;
+****12) 
+SELECT p.cod_projeto, nom_projeto FROM bd_empresa..projeto p 
+FULL OUTER JOIN bd_empresa..alocacao a ON p.cod_projeto = a.cod_projeto;
 
-****13) SELECT num_matricula, nom_empregado FROM bd_empresa..empregado e OUTER JOIN bd_empresa..departamento d ON d.num_matricula_gerente = e.num_matricula;
+****13) 
+SELECT num_matricula, nom_empregado FROM bd_empresa..empregado e OUTER JOIN bd_empresa..departamento d ON d.num_matricula_gerente = e.num_matricula;
 
 14) 
 
